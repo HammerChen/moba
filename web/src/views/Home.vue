14ba9cfcc8c36230920261adc7308ac9b96b34b7
HammerChen <chenxiaochui@126.com>
2020-01-29 12:01:01 +0800

    卡片组件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,30 +2,15 @@
   <div>
     <swiper :options="swiperOption">
       <swiper-slide>
-        <img
-          class="w-100"
-          src="../assets/images/e3bf3b30328abaaef040bf2446158f22.jpeg"
-          alt
-        />
+        <img class="w-100" src="../assets/images/e3bf3b30328abaaef040bf2446158f22.jpeg" alt />
       </swiper-slide>
       <swiper-slide>
-        <img
-          class="w-100"
-          src="../assets/images/14bde65c54b8fe73612292c1f7c35d54.jpeg"
-          alt
-        />
+        <img class="w-100" src="../assets/images/14bde65c54b8fe73612292c1f7c35d54.jpeg" alt />
       </swiper-slide>
       <swiper-slide>
-        <img
-          class="w-100"
-          src="../assets/images/8d2402e6095ca8a7ec4d671a3846bf94.jpeg"
-          alt
-        />
+        <img class="w-100" src="../assets/images/8d2402e6095ca8a7ec4d671a3846bf94.jpeg" alt />
       </swiper-slide>
-      <div
-        class="swiper-pagination pagination-home text-right px-3 pb-2"
-        slot="pagination"
-      ></div>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-2" slot="pagination"></div>
     </swiper>
     <!-- end of swiper -->
 
@@ -43,8 +28,40 @@
     </div>
     <!-- end of nav icons -->
 
-    <i class="iconfont icon-news text-primary"></i>
-
+    <m-card icon="menu1" title="新闻资讯">
+      <div class="nav jc-between">
+        <div class="nav-item active">
+          <div class="nav-link">热门</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">新闻</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">公告</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">活动</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">赛事</div>
+        </div>
+      </div>
+      <div class="pt-3">
+        <swiper>
+          <swiper-slide v-for="m in 5" :key="m">
+            <div class="py-2" v-for="n in 5" :key="n">
+              <span>[新闻]</span>
+              <span>|</span>
+              <span>王者荣耀祝各位召唤师春节快乐！</span>
+              <span>01/24</span>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </m-card>
+    <m-card icon="card-hero" title="英雄列表"></m-card>
+    <m-card icon="menu1" title="精彩视频"></m-card>
+    <m-card icon="menu1" title="图文攻略"></m-card>
   </div>
 </template>
 
